c34c2dae252d44b7fdecc44582bf1018159d6ed3
lopniv <bloginfo639@gmail.com>
2021-02-22 12:47:55 +0700

    First commit

---
+++ app/src/main/java/com/android/sipp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.android.sipp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/android/sipp/ui/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.android.sipp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.sipp.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
