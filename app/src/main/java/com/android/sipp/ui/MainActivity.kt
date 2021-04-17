52c587d36f9706926f8c8ce6d47a508232c70a2a
Lopniv <bloginfo693@gmail.com>
2021-04-17 23:59:34 +0700

    Adding preference to splashscreen and UI Home

--- app/src/main/java/com/android/sipp/ui/MainActivity.kt
+++ app/src/main/java/com/android/sipp/ui/MainActivity.kt
@@ -3,10 +3,15 @@ package com.android.sipp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.sipp.R
+import com.android.sipp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var b: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        b = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
     }
 }
--- app/src/main/java/com/android/sipp/ui/SplashscreenActivity.kt
+++ app/src/main/java/com/android/sipp/ui/SplashscreenActivity.kt
@@ -5,9 +5,13 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.android.sipp.R
+import com.android.sipp.preference.Constants.KEY_USER_ID
+import com.android.sipp.preference.PreferenceManager
 import com.android.sipp.utils.Utils.start
 
 class SplashscreenActivity : AppCompatActivity() {
+
+    private lateinit var pref: PreferenceManager
 
     companion object {
         private const val TIME_LENGTH = 3000
@@ -16,12 +20,21 @@ class SplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+        initiate()
         goToApp()
+    }
+
+    private fun initiate() {
+        pref = PreferenceManager(this)
     }
 
     private fun goToApp() {
         Handler(Looper.getMainLooper()).postDelayed({
-            start(this, IntroActivity::class.java)
+            if (pref.getUserId(KEY_USER_ID) == null){
+                start(this, IntroActivity::class.java)
+            } else {
+                start(this, MainActivity::class.java)
+            }
         }, TIME_LENGTH.toLong())
     }
 }
