674c695b19b98832a69cf78994fd53841fe2a445
Lopniv <bloginfo693@gmail.com>
2021-07-13 13:15:51 +0700

    Update with action menu FAB

--- app/src/main/java/com/android/sipp/ui/activity/driver/DetailDriverPickupActivity.kt
+++ app/src/main/java/com/android/sipp/ui/activity/driver/DetailDriverPickupActivity.kt
@@ -40,5 +40,6 @@ class DetailDriverPickupActivity : AppCompatActivity() {
             b.tvStatusPickup.text = "Sedang dijemput"
         }
         b.tvName.text = order.name
+        b.tvAddress.text = order.address
     }
 }
